03893af28b532f4d13c597ead541b40f8bace57e
Bester <besster.gg@gmail.com>
2022-09-08 14:40:29 -0300

    [Add] Hangman display and word completion display

--- main.py
+++ main.py
@@ -1,50 +1,97 @@
 # Welcome to my simple Hang Game code :D
 import random
-# Later, I'll try import a word list from a txt file to learn how to do it.
-# I saw in stackoverflow
-# def pick_word():
-#     with open("words.txt", "r") as file:
-#         alltext = file.read()
-#         words_list = list(map(str, alltext.split()))
-# print(random.choice(words_list))
-# Also evaluate install random-word package to generate random english words.
+# from words import world_list -> Import list instead create a list here?
+
 def pick_word():
     # Just a example for testing
     word = random.choice(["absence","abuse","account","accident","beneath","bend","benefit","biology","bitter","candidate","campaign","camera","capacity","capture","deaf","daughter","deal","decorate","dialogue","economy","finance","educate","efficient","supportive","elderly","flight","folk","flame","frustration","garbage","gather","gentle","global","hilarious","intelligence","jazz","knife","longevity","momument","nonsense","nobody","turmeric","utilize","sashimi","reconfigure","wheat","yellowish","zone"])
-    return word
+    return word.upper()
 
-
-def display(word):
-    for i in range(0, len(word)):
-        if i != len(word):
-            print("_ ")
-        else:
-            print("_")
-
-
-def check_char():
-    c = ''
-    while len(c) != 1:
-        c = input('Write a character: ')
-        c = c.lower()
-        while ord(c) <= 97:
-            c = input('Write a character, not a number: ')
-            c = c.lower()
-    return c
-
-
-def check_str(word, cvalid):
-    for char in str:
-        if (ord(char) == ord(cvalid)):
-
-
-if __name__ == "__main__":
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
     lives = 6
-    win = False
-    res = ""
+    print("Let's play Hangman Game!")
+    print(display_hangman(lives))
+    print(word_completion)
+    print("\n")
 
+def display_hangman(lives):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[lives]
+
+if __name__ == '__main__':
     word = pick_word()
-    print ("Estoy en main: {}".format(word))
-    cvalid = check_char()
-    print("Caracter valido: {}".format(cvalid))
-    display(word)
+    play(word)
