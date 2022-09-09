396288b5c40d96a93e1cf7c3194fbd63e95ba8c8
Bester <besster.gg@gmail.com>
2022-09-09 13:26:54 -0300

    [Add] Functional Game but have a short list of words

--- main.py
+++ main.py
@@ -17,6 +17,45 @@ def play(word):
     print(display_hangman(lives))
     print(word_completion)
     print("\n")
+    while not guessed and lives > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "Is not in the word.")
+                lives -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job, " + guess + " is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print("{}is not the word.".format(guess))
+                lives -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        print(display_hangman(lives))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
 
 def display_hangman(lives):
     stages = [  # final state: head, torso, both arms, and both legs
@@ -92,6 +131,13 @@ def display_hangman(lives):
     ]
     return stages[lives]
 
-if __name__ == '__main__':
+def main():
     word = pick_word()
     play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = pick_word()
+        play(word)
+
+
+if __name__ == "__main__":
+    main()
